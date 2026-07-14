e703de7881228f62955c1985bc3680c1b070c910
Joshitha A N <joshithaan.dev@gmail.com>
2026-07-14 11:12:43 +0530

    Delete src directory

--- src/main/java/com/cognizant/ormlearn/repository/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.ormlearn.repository;
-
-import com.cognizant.ormlearn.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-
-}
